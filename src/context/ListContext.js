279229571d4bd353866b8717c2c964dc4750c4b1
vivek <viveksinghmongare@gmail.com>
2023-01-04 11:21:28 +0530

    contextProvider for todo list

---
+++ src/context/ListContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+export const ListContext = createContext();
+
+const ListProvider = (props) => {
+  const [toDoList, setToDoList] = useState([]);
+  const addTask = (task) => {
+    setToDoList([...toDoList, task]);
+  };
+  const addAllTask = (tasks) => {
+    setToDoList([...toDoList, ...tasks]);
+  };
+  const removeTask = (id) => {
+    const tempList = [...toDoList];
+    tempList.splice(id, 1);
+    setToDoList(tempList);
+  };
+  return (
+    <ListContext.Provider value={(toDoList, addAllTask, addTask, removeTask)}>
+      {props.children}
+    </ListContext.Provider>
+  );
+};
